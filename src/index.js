5326335f8d03ad9782e3c60db3d38a78bea08e61
Annakim219 <97003692+Annakim219@users.noreply.github.com>
2022-09-13 22:01:21 +0900

    switch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
+import { act } from "@testing-library/react";
 import { createStore } from "redux";
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countReducer = (count = 0, action) => {
   // count가 undifined되지 않도록 initialization
   // reducer는 나의 data를 modified하는 함수.
-  // action은 reducer와 소통하기 위한 방법.
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  // -> return하는 것은 무엇이든 application의 state가 됨.
+  // action은 reducer와 소통하기 위한 방법. action은 odject여야함. string X, 무조건 type이 있어야함.
+
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
+
+  //   if (action.type === "ADD") {
+  //     return count + 1;
+  //   } else if (action.type === "MINUS") {
+  //     return count - 1;
+  //   } else {
+  //     return count;
 };
 
 const countStore = createStore(countReducer);
@@ -25,7 +39,8 @@ const onChange = () => {
 };
 
 countStore.subscribe(onChange);
-// subscribe : store에 변화가 있을 때 감지해서 함수를 불러옴.
+// subscribe : store에 변화가 있을 때 감지해서 function을 불러옴.
 
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+// reducer에게 dispatch를 통해 action을 보낸다.
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
