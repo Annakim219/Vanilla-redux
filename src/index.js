5f48d33149ce65954f3f35cce2bed1d82479e0e6
Annakim219 <97003692+Annakim219@users.noreply.github.com>
2022-09-13 21:46:59 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,52 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countReducer = (count = 0, action) => {
+  // count가 undifined되지 않도록 initialization
+  // reducer는 나의 data를 modified하는 함수.
+  // action은 reducer와 소통하기 위한 방법.
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
+const countStore = createStore(countReducer);
+// store는 나의 data를 저장하는 곳.
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
-const handleMinus = () => {
-  count--;
-  updateText();
-};
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// dispatch는 action을 실행하기 위한 메세지를 보내는 것.
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
+
+// let count = 0;
+
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// };
+
+// const handleAdd = () => {
+//   count++;
+//   updateText();
+// };
+// const handleMinus = () => {
+//   count--;
+//   updateText();
+// };
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
